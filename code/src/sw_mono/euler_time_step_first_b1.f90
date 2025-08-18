21ce215d048e6f892e8de36c1ecebc8a6eb9f567
mnhleu <helleu.maiann@orange.fr>
2025-08-18 15:08:26 +0200

    euler time step

--- code/src/sw_mono/euler_time_step_first_b1.f90
+++ code/src/sw_mono/euler_time_step_first_b1.f90
@@ -52,7 +52,7 @@
 !! \brief This file includes euler_time_step_first_b1 routine.
 !! \details The file includes only euler_time_step_first_b1 routine (see doc euler_time_step_first_b1 routine).
 
-SUBROUTINE euler_time_step_first_b1_poro( dof , mesh )
+SUBROUTINE euler_time_step_first_b1( dof , mesh )
    USE m_common
    USE m_mesh
    USE m_mpi
@@ -415,7 +415,7 @@ SUBROUTINE euler_time_step_first_b1_poro( dof , mesh )
    call com_dof( dof , mesh )
    call com_var_r( bathy_cell , mesh ) ! Required MPI Communication due to inverse variable dependency
 
-END SUBROUTINE euler_time_step_first_b1_poro
+END SUBROUTINE euler_time_step_first_b1
 
 
 
