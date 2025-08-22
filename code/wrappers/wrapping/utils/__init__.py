b7f367d48448eb41fb2836fa3f7859990393df74
mnhleu <helleu.maiann@orange.fr>
2025-08-22 15:24:47 +0200

    ignore obj, tap, wrappers

--- code/wrappers/wrapping/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-# ~ from .PepsiNetCDF import PepsiNetCDF as PepsiNetCDF
-# ~ from .SwotObs import SwotObs as SwotObs
-# ~ from .mesh_from_swot_observations import mesh_from_swot_observations
-# ~ from .pepsi_nodes_observations import pepsi_nodes_observations
